4366aaa07e29d0727f4ffe0ca545193c9169b9a7
Enes Çetintaş <enesctnts@gmail.com>
2022-03-18 20:33:56 +0300

    18 Mart Commit 5

---
+++ RentACarProject/Business/BusinessAspects/Autofac/SecuredOperation.cs
@@ -0,0 +1,39 @@
+﻿using Core.Utilities.Interceptors;
+using Core.Utilities.IoC;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Castle.DynamicProxy;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Business.BusinessAspects.Autofac
+{
+
+    public class SecuredOperation : MethodInterception
+    {
+        private string[] _roles;
+        private IHttpContextAccessor _httpContextAccessor; //JWT ye istek yaptıgımızda o anda binlerce kişi istek yapabilir. Her bir istek için HttpContext i oluşur
+
+        public SecuredOperation(string roles)
+        {
+            _roles = roles.Split(',');//Split komutu belirttiğimiz kelimeye göre belirleyip array e atıyor. yani ' , ' bunu görene kadar kısmı array bir elemanmış gibi at ' , ' den sonrasını yine bir elemanmış gibi array e at.
+            _httpContextAccessor = ServiceTool.ServiceProvider.GetService<IHttpContextAccessor>();//Aspect lerde DependencyInjection yapılamıyor. örnek olarak controller business çagırır business dal ı çagırır. Aspect bu düzende yoktur olmadıgı için bu şekilde onları otomatik çagıran sistem yapıyoruz.ServiceTool classıyla otomatik DependencyInjection yaptırabiliyoruz.
+
+        }
+
+        protected override void OnBefore(IInvocation invocation)
+        {
+            var roleClaims = _httpContextAccessor.HttpContext.User.ClaimRoles();
+            foreach (var role in _roles)
+            {
+                if (roleClaims.Contains(role))
+                {
+                    return;
+                }
+            }
+            throw new Exception(Messages.AuthorizationDenied);
+        }
+    }
+}
---
+++ RentACarProject/Core/Utilities/IoC/ServiceTool.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.IoC
+{
+    public static class ServiceTool
+    {
+        //Burası bizim webapi veya autofac de oluşturdugumuz injectionları oluşturabilmemize yarıyor.
+        //Aspect lerde DependencyInjection yapılamıyor. örnek olarak controller business çagırır business dal ı çagırır. Aspect bu düzende yoktur olmadıgı için bu şekilde onları otomatik çagıran sistem yapıyoruz.
+        public static IServiceProvider ServiceProvider { get; private set; }
+
+        public static IServiceCollection Create(IServiceCollection services)
+        {
+            ServiceProvider = services.BuildServiceProvider();
+            return services;
+        }
+    }
+}
